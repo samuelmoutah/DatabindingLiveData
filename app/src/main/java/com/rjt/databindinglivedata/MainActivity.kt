a6e88df783ccc6e1ed219d909b8148342c163aa2
Sungjin Mo <samuelmo78@gmail.com>
2020-02-10 02:57:04 -0600

    first commit

---
+++ app/src/main/java/com/rjt/databindinglivedata/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.rjt.databindinglivedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.rjt.databindinglivedata.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProviders.of(this)
+            .get(MainViewModel::class.java)
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply {
+            this.setLifecycleOwner(this@MainActivity)
+            this.viewmodel = mainViewModel
+        }
+
+        mainViewModel.editTextContent.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+}
